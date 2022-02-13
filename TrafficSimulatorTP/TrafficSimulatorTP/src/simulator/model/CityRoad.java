d7eee4afec5a5841c50c29b658c0fdb0b321486a
BonitoFish <99401370+BonitoFish@users.noreply.github.com>
2022-02-13 21:37:53 +0100

    Modificacion CityRoad del metodo reduceTotalContamination con un pequeño cambio

--- TrafficSimulatorTP/TrafficSimulatorTP/src/simulator/model/CityRoad.java
+++ TrafficSimulatorTP/TrafficSimulatorTP/src/simulator/model/CityRoad.java
@@ -8,16 +8,15 @@ public class CityRoad extends Road{
 
 	@Override
 	void reduceTotalContamination() {
-		// TODO Auto-generated method stub
-		String we =getWeather().toString().toUpperCase();
+
+		String we = getWeather().toString();
+		
 		if(we.equals("WINDY")||we.equals("STORM")) {
 			reduceContamination(10);
 		}
 		else {
 			reduceContamination(2);
-		}
-		
-		
+		}		
 	}
 
 	@Override
@@ -27,8 +26,9 @@ public class CityRoad extends Road{
 
 	@Override
 	int calculateVehicleSpeed(Vehicle v) {
-		int f =v.getContClass();
-		int s= super.getMaxSpeed();
+		
+		int f = v.getContClass();
+		int s = getMaxSpeed();		
 		return (int)(((11.0-f )/11.0)*s);
 	}
 
