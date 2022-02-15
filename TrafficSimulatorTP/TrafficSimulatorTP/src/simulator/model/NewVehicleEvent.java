978f04834be6f96209db1be42fdb356122cc7c58
Santiago Castro Mateos <santiagocastromateos@gmail.com>
2022-02-15 14:14:07 +0100

    creado NewVehicleEvent y hechos su metodos, revisar la lista que se usa (que preserve el orden) y mirar si hace falta exception

---
+++ TrafficSimulatorTP/TrafficSimulatorTP/src/simulator/model/NewVehicleEvent.java
@@ -0,0 +1,39 @@
+package simulator.model;
+
+import java.util.LinkedList;
+import java.util.List;
+
+public class NewVehicleEvent extends Event {
+	
+	private String id;
+	private int maxSpeed, contClass;
+	private List<String> itinerary;
+	
+	NewVehicleEvent(int time, String id, int maxSpeed, int contClass, List<String> itinerary) {
+		super(time);
+		// TODO Auto-generated constructor stub
+		this.id= id;
+		this.maxSpeed=maxSpeed;
+		this.contClass=contClass;
+		this.itinerary=itinerary;
+	}
+
+	@Override
+	void execute(RoadMap map) {
+		// TODO Auto-generated method stub
+		if(map.getVehicle(id)==null) {
+			List<Junction> listJun= new LinkedList<Junction>();
+			for(int i=0;i<map.getJunctions().size();i++) {
+				Junction j=map.getJuction(itinerary.get(i));
+					if(j!= null) {
+						listJun.add(j);
+					}
+					else {
+						//TODO exception "ALARMA, ITINERARIO NO VALIDO XD"
+					}
+			}
+			map.addVehicle(new Vehicle(id, contClass, contClass,listJun));
+		}
+	}
+
+}
