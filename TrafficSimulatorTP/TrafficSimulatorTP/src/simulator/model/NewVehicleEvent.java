717fa19db9069880002be944cbb0912726e7c689
BonitoFish <99401370+BonitoFish@users.noreply.github.com>
2022-02-19 14:09:06 +0100

    cambios en execute()

--- TrafficSimulatorTP/TrafficSimulatorTP/src/simulator/model/NewVehicleEvent.java
+++ TrafficSimulatorTP/TrafficSimulatorTP/src/simulator/model/NewVehicleEvent.java
@@ -11,30 +11,28 @@ public class NewVehicleEvent extends Event {
 	
 	NewVehicleEvent(int time, String id, int maxSpeed, int contClass, List<String> itinerary) {
 		super(time);
-		this.id= id;
-		this.maxSpeed=maxSpeed;
-		this.contClass=contClass;
-		this.itinerary=itinerary;
+		this.id = id;
+		this.maxSpeed = maxSpeed;
+		this.contClass = contClass;
+		this.itinerary = itinerary;
 	}
 
 	@Override
 	void execute(RoadMap map) {
-		if(map.getVehicle(id)==null) {
-			List<Junction> listJun= new LinkedList<Junction>();
-			for(int i=0;i<map.getJunctions().size();i++) {
-				Junction j=map.getJuction(itinerary.get(i));
-					if(j!= null) {
-						listJun.add(j);
-					}
-					else {
-						throw new IllegalArgumentException("Invalid Itinerary");
-					}
+		
+		
+		List<Junction> listJun = new LinkedList<Junction>();
+		for(String s: itinerary) {
+			Junction j = map.getJuction(s);
+			if(j != null) {
+				listJun.add(j);
 			}
-			Vehicle v = new Vehicle(id, maxSpeed, contClass,listJun);
-			map.addVehicle(v);
-			v.moveToNextRoad();
-			
 		}
+		
+		Vehicle v = new Vehicle(id, maxSpeed, contClass, listJun);
+		map.addVehicle(v);
+		v.moveToNextRoad();	
+					
 	}
 
 }
